f7787e908ebc0ee27442d44b8dfce97777d418a6
Luiz Chamon <luizchamon@gmail.com>
2022-12-09 18:19:27 -0300

    Include arxiv links in CV
    Signed-off-by: Luiz Chamon <luizchamon@gmail.com>

--- .github/workflows/bib_to_website.py
+++ .github/workflows/bib_to_website.py
@@ -3,7 +3,6 @@
 import os, re, sys
 
 PATH = os.path.dirname(os.path.realpath(__file__))
-print(PATH)
 BIB_PATH = os.path.join(PATH,'../../../publications-list')
 WEB_PATH = os.path.join(PATH,'../../www')
 
@@ -21,17 +20,19 @@ os.rename(os.path.join(BIB_PATH, "temp.bib"), os.path.join(BIB_PATH, "publicatio
 
 # Prepare bib file for CV
 author_regex = re.compile(r'L\. F\. O\. Chamon(\*?)')
-arxiv_regex = re.compile(r'arxiv\s*=.*')
+arxiv_regex = re.compile(r'arxiv\s*=(.*)')
 pdf_regex = re.compile(r'pdf\s*=.*')
 poster_regex = re.compile(r'poster\s*=.*')
 slides_regex = re.compile(r'slides\s*=.*')
-url_regex = re.compile(r'url\s*=.*')
+video_regex = re.compile(r'video\s*=.*')
+url_regex = re.compile(r'\\url\{(.*)\}')
 with open(os.path.join(BIB_PATH, 'publications.bib'), 'r') as input_file, open(os.path.join(BIB_PATH, 'cv.bib'), 'w') as output_file:
     for line in input_file:
         str = re.sub(author_regex, r'\\textbf{L. F. O. Chamon}\1', line)
-        str = re.sub(arxiv_regex, '', str)
+        str = re.sub(arxiv_regex, r'url=\1', str)
         str = re.sub(pdf_regex, '', str)
         str = re.sub(poster_regex, '', str)
         str = re.sub(slides_regex, '', str)
-        str = re.sub(url_regex, '', str)
+        str = re.sub(video_regex, '', str)
+        str = re.sub(url_regex, r'\1', str)
         output_file.write(str)
